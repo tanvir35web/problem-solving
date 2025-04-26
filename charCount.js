c19c1f3524416c86e6095983f4330d2f3aec2449
tanvir35web <islammdtanvirul@gmail.com>
2025-04-27 00:14:12 +0600

    charCount multiple way performance test added

--- charCount.js
+++ charCount.js
@@ -1,8 +1,7 @@
-const charCount = (str) => {
-  const count = {}
+const charCountOld = (str) => {
+  const count = {};
   for (let i = 0; i < str.length; i++) {
     const char = str[i].toLowerCase();
-    
     if (count[char]) {
       count[char]++;
     } else {
@@ -10,7 +9,35 @@ const charCount = (str) => {
     }
   }
   return count;
-}
+};
 
-const str = "Banana"
-console.log(charCount(str));
+const charCountNew = (str) => {
+  const count = {};
+  for (const char of str.toLowerCase()) {
+    count[char] = (count[char] ?? 0) + 1;
+  }
+  return count;
+};
+
+const charCountWithMap = (str) => {
+  const count = new Map();
+  for (const char of str.toLowerCase()) {
+    count.set(char, (count.get(char) ?? 0) + 1);
+  }
+  return Object.fromEntries(count);
+};
+
+const str = "Banana".repeat(1000000);
+
+// Test
+console.time("Old For Loop");
+charCountOld(str);
+console.timeEnd("Old For Loop");
+
+console.time("New For-Of Loop");
+charCountNew(str);
+console.timeEnd("New For-Of Loop");
+
+console.time("Map Version");
+charCountWithMap(str);
+console.timeEnd("Map Version");
